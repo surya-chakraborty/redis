3d5caee063858ed76fce28fdd02a117683040d8f
Surya Chakraborty <suryachakraborty.dev@gmail.com>
2026-05-17 19:25:18 +0530

    real email jobqueue added using resend service

--- bullmq/src/worker.js
+++ bullmq/src/worker.js
@@ -1,18 +1,39 @@
 const { Worker } = require('bullmq')
 const { connection } = require('./queue.js')
+const { Resend } = require('resend')
+require('dotenv').config()
+
 
 // What this file consist of ? worker code, made from worker object of bullmq with name, connection and job simulation
 // then on complted or failed mission
+
+// updated the code structiure mainly in the processor section for real email jobs using resend.com
+const resend = new Resend(process.env.RESEND_API_KEY)
 
 const worker = new Worker(
     "emails",
     // processor code 
     async function(job){
-        console.log("processing email jobs", job.id, job.name, job.data)
+        console.log("processing email jobs", job.id)
         
-        await new Promise((resolve) => setTimeout(resolve, 1500))
+        // await new Promise((resolve) => setTimeout(resolve, 1500))
+        const response = await resend.emails.send({
+            from: 'onboarding@resend.dev',
+            to: job.data.to,
+            subject: 'Welcome to bull mq real email job queue list, well done!',
+            html: `
+                <h1>Hello ${job.data.name}</h1>
+                
+                <p>You have got this mail as you succefully been sent the email via email job queue made by surya chakraborty with the help of 'resend.com'
+                <br>
+                For more information contact: surya chakraborty
+                <br>
+                you already got his number!!
+                </p>
+            `
+        })
 
-        console.log("Email job completed!", job.id, job.name, job.data)
+        console.log("Email sent!", response.data?.id)
         
     },
     {   
